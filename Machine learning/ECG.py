81f70ce3992fca1549a0ea2112f2e2dc668a18a5
H4naka <102389323+H4naka@users.noreply.github.com>
2023-05-25 13:56:52 +0200

    Update ECG.py

--- Machine learning/ECG.py
+++ Machine learning/ECG.py
@@ -7,104 +7,6 @@ import sys
 from scipy.signal import freqz
 sys.path.append("/usr/local/lib/python3.7/site-packages")
 
-
-# # =================loading data================================================
-# # 
-# """We need to define the dataset path and the subject ID"""
-# #
-# data_set_path = "/Users/kakis/OneDrive/Documenten/unie/Delft/BSc Electrical Engineering/Year 2/EPO/EPO-4/WESAD/WESAD/"
-# subject = 'S2'# --- 'S17'
-# #
-# """The class below allows you to access all the recordings and labels in the dataset."""
-# #
-# #
-# class read_data_of_one_subject:
-#     """Read data from WESAD dataset"""
-#     def __init__(self, path, subject):
-#         self.keys = ['label', 'subject', 'signal']
-#         self.signal_keys = ['wrist', 'chest']
-#         self.chest_sensor_keys = ['ACC', 'ECG', 'EDA', 'EMG', 'Resp', 'Temp']
-#         self.wrist_sensor_keys = ['ACC', 'BVP', 'EDA', 'TEMP']
-#         #os.chdir(path)
-#         #os.chdir(subject)
-#         with open(path + subject +'/'+subject + '.pkl', 'rb') as file:
-#             data = pickle.load(file, encoding='latin1')
-#         self.data = data
-
-
-#     def get_labels(self):
-#         return self.data[self.keys[0]]
-    
-
-#     def get_wrist_data(self):
-#         """"""
-#         #label = self.data[self.keys[0]]
-#         assert subject == self.data[self.keys[1]]
-#         signal = self.data[self.keys[2]]
-#         wrist_data = signal[self.signal_keys[0]]
-#         #wrist_ACC = wrist_data[self.wrist_sensor_keys[0]]
-#         #wrist_ECG = wrist_data[self.wrist_sensor_keys[1]]
-#         return wrist_data
-
-#     def get_chest_data(self):
-#         """"""
-#         signal = self.data[self.keys[2]]
-#         chest_data = signal[self.signal_keys[1]]
-#         return chest_data
-# #    
-# # Object instantiation
-# obj_data = {}
-# # 
-# # Accessing class attributes and method through objects
-# obj_data[subject] = read_data_of_one_subject(data_set_path, subject)
-# #
-# """The code below allows you to read and print the length of all biosignals from the chest recording device recorded all at 700 Hz."""
-# #
-# chest_data_dict = obj_data[subject].get_chest_data()# your code here
-# chest_dict_length = {key: len(value) for key, value in chest_data_dict.items()}
-# print(chest_dict_length)
-# #
-# """You can also access the labels of each class. The following labels are provided: 0 = not defined / transient, 1 = baseline, 2 = stress, 3 = amusement, 4 = meditation, 5/6/7 = should be ignored in this dataset."""
-# #
-# # Get labels
-# labels = obj_data[subject].get_labels()
-# baseline = np.asarray([idx for idx,val in enumerate(labels) if val == 1])
-# stress = np.asarray([idx for idx,val in enumerate(labels) if val == 2])
-# amusement = np.asarray([idx for idx,val in enumerate(labels) if val == 3])
-# meditation = np.asarray([idx for idx,val in enumerate(labels) if val == 4])
-# #
-# #
-# """Let's load some part of the ECG signal during baseline recording"""
-# #
-# fs=700
-# ecg_base=chest_data_dict['ECG'][baseline,0]
-# ecg_stress=chest_data_dict['ECG'][stress,0]
-# ecg_amusement=chest_data_dict['ECG'][amusement,0]
-# ecg_meditation=chest_data_dict['ECG'][meditation,0]
-# # 
-# # =============================================================================
-# ECG = {}
-
-# #read cvs file
-# data = pd.read_csv("EMG_and_ECG.csv")
-# data
-# print(data.shape)
-# print(data)
-
-# # seperate file into EMG and ECG
-# timestamp = data['TimeStamp']
-# EMG = data['EMG Data']
-# ECG = data['ECG Data']
-# ecg_base = ECG
-# #plot ECG
-# plt.figure(figsize=(12,4))
-# plt.plot(timestamp,ECG, label="raw ECG")
-# plt.xlabel('$Time (s)$') 
-# plt.ylabel('$ECG$') 
-# plt.legend()
-# plt.show()
-
-# fs=200
 class ECGprep:
     def __init__ (self,Fs,ecg_class=[],title=""): 
         self.title = title
@@ -123,13 +25,14 @@ class ECGprep:
         # cut a smaller window      
         if title=="":
             title=self.title
-        plt.figure(figsize=(12,4))
-        plt.plot(t,ecg)
-        plt.title("input ECG "+ title)
-        plt.xlabel('$Time (s)$') 
-        plt.xlim(0,max(t))
-        plt.ylabel('$ECG$')
-        plt.show()  
+#         plt.figure(figsize=(12,4))
+#         plt.plot(t,ecg)
+#         plt.title("input ECG "+ title)
+#         plt.xlabel('$Time (s)$') 
+#         plt.xlim(0,max(t))
+#         plt.ylabel('$ECG$')
+#         plt.show()  
+        
     def filtering_data(self,ecg_class=1,title=""):
         nyq = 0.5*self.Fs
         order=5
@@ -150,9 +53,8 @@ class ECGprep:
         
         w, h = freqz(b, a, fs=self.Fs)
         
-        fig, ax = plt.subplots(1, 2, figsize=(15,8))
-        
-        ax[0].plot(w/2/np.pi*self.Fs,abs(h))
+#         plt.figure()
+#         plt.plot(w/2/np.pi*self.Fs,abs(h))
 
         # lowpass filter
         low=70
@@ -162,15 +64,15 @@ class ECGprep:
         
         w, h = freqz(b, a, fs=self.Fs)
         
-        ax[0].plot(w/2/np.pi*self.Fs,abs(h))
-        ax[0].set_title("Butterworth bandpass filter frequency domain")
-        ax[0].set_xlabel("Frequency [Hz]")
-        ax[0].set_ylabel("ECG")
-        ax[0].set_xscale("log")
-        ax[0].margins(0,0.1)
-        ax[0].grid(which="both", axis="both")
-        ax[0].axvline(high*nyq, color="green")
-        
+#         plt.plot(w/2/np.pi*self.Fs,abs(h))
+#         plt.title("Butterworth bandpass filter")
+#         plt.xlabel("Frequency [Hz]")
+#         plt.ylabel("ECG")
+#         plt.xscale("log")
+#         plt.margins(0,0.1)
+#         plt.grid(which="both", axis="both")
+#         plt.axvline(high*nyq, color="green")
+#         plt.show()
 
         # notch filter remove powerline interference
         notch=50
@@ -180,36 +82,37 @@ class ECGprep:
         
         w, h = freqz(b, a, fs=self.Fs)
         
-        ax[1].plot(w/2/np.pi*self.Fs,abs(h))
-        ax[1].set_title("Butterworth bandpass and notch filter frequency domain")
-        ax[1].set_xlabel("Frequency [Hz]")
-        ax[1].set_ylabel("$ECG$")
-        ax[1].set_xscale("log")
-        ax[1].margins(0,0.1)
-        ax[1].grid(which="both", axis="both")
-        ax[1].axvline(high*nyq, color="green")
-        plt.show()
+#         plt.figure()
+#         plt.plot(w/2/np.pi*self.Fs,abs(h))
+#         plt.title("Notch filter ")
+#         plt.xlabel("Frequency [Hz]")
+#         plt.ylabel("$ECG$")
+#         plt.xscale("log")
+#         plt.margins(0,0.1)
+#         plt.grid(which="both", axis="both")
+#         plt.axvline(high*nyq, color="green")
+#         plt.show()
         
 
-        plt.figure(figsize=(12,4))
-        plt.plot(t,ecg,label="raw ECG")
-        plt.plot(t,ecg_hln, label="filtered ECG")
-        if title=="":
-            title=self.title
-        plt.title("ECG filtering "+ title)
-        plt.xlabel('$Time (s)$') 
-        plt.xlim(0,max(t))
-        plt.ylabel('$ECG$') 
-        plt.legend()
-        plt.show()
+#         plt.figure(figsize=(12,4))
+#         plt.plot(t,ecg,label="raw ECG")
+#         plt.plot(t,ecg_hln, label="filtered ECG")
+#         if title=="":
+#             title=self.title
+#         plt.title("ECG filtering "+ title)
+#         plt.xlabel('$Time (s)$') 
+#         plt.xlim(0,max(t))
+#         plt.ylabel('$ECG$') 
+#         plt.legend()
+#         plt.show()
 
-        plt.figure(figsize=(12,4))
-        plt.plot(t,ecg_hln)
-        plt.title("ECG filtered "+title)
-        plt.xlabel('$Time (s)$') 
-        plt.xlim(0,max(t))
-        plt.ylabel('$ECG$') 
-        plt.show()
+#         plt.figure(figsize=(12,4))
+#         plt.plot(t,ecg_hln)
+#         plt.title("ECG filtered "+title)
+#         plt.xlabel('$Time (s)$') 
+#         plt.xlim(0,max(t))
+#         plt.ylabel('$ECG$') 
+#         plt.show()
         return ecg_hln
 
 # ECG = ECGprep(fs,ecg_base,"baseline")
@@ -219,20 +122,7 @@ class ECGprep:
 
 # ECG = ECGprep(fs,ecg_stress,"stress")
    
-# ECG.plotdata()
-# ecg_filt = ECG.filtering_data()
 
-# ECG = ECGprep(fs,ecg_amusement,"amusement")
-   
-# ECG.plotdata()
-# ecg_filt = ECG.filtering_data()
-
-# ECG = ECGprep(fs,ecg_meditation,"meditation")
-   
-# ECG.plotdata()
-# ecg_filt = ECG.filtering_data()
-
-ECGfeat = {}
 class ECGfeatures:
     def __init__(self,ecg_filt,Fs,title=""):
         self.ecg = ecg_filt
@@ -241,16 +131,17 @@ class ECGfeatures:
         
     def rpeaks(self,title=""):
         _, rpeaks = nk.ecg_peaks(self.ecg, sampling_rate=self.Fs) 
-        if title=="":
-            title=self.title
-        plt.plot((rpeaks['ECG_R_Peaks']/self.Fs),self.ecg[rpeaks['ECG_R_Peaks']], 'go')
-        t=np.arange(0,self.ecg.size*(1/self.Fs),(1/self.Fs))
-        plt.plot(t,self.ecg)
-        plt.title("R-peaks "+ title)
-        plt.xlim(0,max(t))
-        plt.xlabel('$Time (s)$') 
-        plt.ylabel('$ECG$') 
-        plt.show()
+        # if title=="":
+        #     title=self.title
+        # plt.plot((rpeaks['ECG_R_Peaks']/self.Fs),self.ecg[rpeaks['ECG_R_Peaks']], 'go')
+        # t=np.arange(0,self.ecg.size*(1/self.Fs),(1/self.Fs))
+        # plt.plot(t,self.ecg)
+        # plt.title("R-peaks "+ title)
+        # plt.xlim(0,max(t))
+        # plt.xlabel('$Time (s)$') 
+        # plt.ylabel('$ECG$') 
+        # plt.show()
+        return rpeaks
         
 # ECGfeat = ECGfeatures(ecg_filt,fs,"test")
 # ECGfeat.rpeaks("baseline")
