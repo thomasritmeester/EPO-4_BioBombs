47d07d9f95cdbbec7a1e3ac29dd8dfc69655f4a3
H4naka <102389323+H4naka@users.noreply.github.com>
2023-05-25 13:53:02 +0200

    Update EDA.py

--- Machine learning/EDA.py
+++ Machine learning/EDA.py
@@ -1,15 +1,7 @@
-# import os
-# from os import listdir
-# from os.path import isfile, join, isdir
-import pickle
 import numpy as np
-# from scipy import signal as sig
-# import numpy as np
-#import matplotlib.pyplot as plt
-import sys
+import matplotlib.pyplot as plt
 import scipy.ndimage
-#sys.path.append("/usr/local/lib/python3.7/site-packages")
-
+from scipy.signal import chirp, find_peaks, peak_widths, peak_prominences
 
 class EDAprep:
     def __init__ (self,Fs,eda_class, minutes,title):
@@ -23,12 +15,13 @@ class EDAprep:
         # cut a smaller window      
         if title=="":
             title=self.title
-        '''
+        plt.figure()
         plt.title("input EDA "+ title)
         plt.plot(self.t,self.eda)
         plt.xlabel('$Time (s)$') 
+        plt.xlim(0,max(self.t))
         plt.ylabel('$EDA$')
-        plt.show()'''
+        plt.show()
     
     def filtering_data(self,title=""):
         ############ lowpass filtering
@@ -40,17 +33,19 @@ class EDAprep:
         b, a = scipy.signal.butter(order, frequency, btype='low')
         eda_lp = scipy.signal.filtfilt(b, a, self.eda)
         
-        '''# plot
-        if title=="":
-            title=self.title
-        plt.title("EDA lowpass filtered "+ title)
-        plt.plot(self.t,self.eda)
-        plt.plot(self.t,eda_lp)
-        plt.ylim(np.min(eda_lp)-1,np.max(eda_lp)+1)
-        # labels and titles
-        plt.xlabel('$Time (s)$') 
-        plt.ylabel('$EDA$') 
-        plt.show()'''
+        # plot
+#         if title=="":
+#             title=self.title
+#         plt.figure()
+#         plt.title("EDA lowpass filtered "+ title)
+#         plt.plot(self.t,self.eda)
+#         plt.plot(self.t,eda_lp)
+#         plt.ylim(np.min(eda_lp)-1,np.max(eda_lp)+1)
+#         plt.xlim(0,max(self.t))
+#         # labels and titles
+#         plt.xlabel('$Time (s)$') 
+#         plt.ylabel('$EDA$') 
+#         plt.show()
         return eda_lp
     
     def smoothing_data(self,eda_lp, title=""):
@@ -76,19 +71,21 @@ class EDAprep:
         eda_sm = np.convolve(w, eda_sm0, mode='same')# your code here
         # eda_sm = eda_sm[:len(self.eda)]
         eda_sm = eda_sm[size:-size]
+        self.smooth = eda_sm
 
         
-        '''# plot
-        if title=="":
-            title=self.title
-        plt.title("EDA smoothed "+ title)
-        plt.plot(self.t,self.eda)
-        plt.plot(self.t,eda_sm)
-        #plt.xlim(70, 90)
-        # labels and titles
-        plt.xlabel('$Time (s)$') 
-        plt.ylabel('$EDA$') 
-        plt.show()'''
+        # plot
+#         if title=="":
+#             title=self.title
+#         plt.figure()
+#         plt.title("EDA smoothed "+ title)
+#         plt.plot(self.t,self.eda)
+#         plt.plot(self.t,eda_sm)
+#         plt.xlim(0,max(self.t))
+#         # labels and titles
+#         plt.xlabel('$Time (s)$') 
+#         plt.ylabel('$EDA$') 
+#         plt.show()
         
         return eda_sm
     
@@ -100,58 +97,118 @@ class EDAprep:
         freqs=[0.05]
         sos = scipy.signal.butter(order, freqs, btype='high', output="sos", fs= self.Fs)
         phasic= scipy.signal.sosfiltfilt(sos, eda_sm)
+        self.phasic = phasic
         
         #Tonic
         order=5
         freqs=[0.05]
         sos = scipy.signal.butter(order, freqs, btype='low', output="sos",  fs= self.Fs)
         tonic= scipy.signal.sosfiltfilt(sos, eda_sm)
+        self.tonic = tonic
         
-        '''# plot
-        if title=="":
-            title=self.title
-        plt.title("EDA meer "+ title)
-        plt.plot(self.t,eda_sm,label='smooth_eda')
-        plt.plot(self.t,phasic,label='phasic')
-        plt.plot(self.t,tonic,label='tonic')
-        # labels and titles
-        plt.xlabel('$Time (s)$') 
-        plt.ylabel('$EDA$') ()
-        plt.legend()
-        plt.show()'''
+        # plot
+#         if title=="":
+#             title=self.title
+#         plt.figure()
+#         plt.title("EDA meer "+ title)
+#         plt.plot(self.t,eda_sm,label='smooth_eda')
+#         plt.plot(self.t,phasic,label='phasic')
+#         plt.plot(self.t,tonic,label='tonic')
+#         plt.xlim(0,max(self.t))
+#         # labels and titles
+#         plt.xlabel('$Time (s)$') 
+#         plt.ylabel('$EDA$') 
+#         plt.legend()
+#         plt.show()
         
         return eda_sm, phasic, tonic
+    
+    def eda_phasic(self):
+        return self.phasic
+    
+    def eda_tonic(self):
+        return self.tonic
+    
+class EDAfeatures:
+    def __init__(self,Fs):
+        self.Fs = Fs
+        
+    def calc_phasic_data(self,phasic, peak, height):
+        ##Find all the points on the plot below the 50% of the peak
+        half_points = np.where(((phasic - (phasic[peak] - 0.5*height) < 0.00001)))[0]
+        
+        ##Finds the index of the point directly to the right of the peak
+        half_amp_index = np.inf
+        for j in half_points:
+            if(j < half_amp_index and j > peak):
+                half_amp_index = j
 
-'''
-EDA = {}
-#EDA = EDAprep(eda_base,20,"baseline")
+        ##Calculate onset and offset
+        onset_points = np.where(((phasic - (phasic[peak] - 0.63*height) < 0.00001)))[0]
+        
+        ##Finds the index of the point directly to the right of the peak
+        offset_amp_index = np.inf
+        for j in onset_points:
+            if(j < offset_amp_index and j > peak):
+                offset_amp_index = j
+        
+        ##Finds the index of the point directly to the right of the peak
+        onset_amp_index = 0
+        for j in onset_points:
+            if(j > onset_amp_index and j < peak):
+                onset_amp_index = j
+        return onset_amp_index, half_amp_index, offset_amp_index 
+    
+    def calc_phasic_features(self, phasic, t_tot, state):
+  
+        temp_array = np.asarray([], dtype = "float")
+        
+        
+        orienting_mag = np.asarray([], dtype = "float")
+        orienting_time = np.asarray([], dtype = "float")
+        half_recov_time = np.asarray([], dtype = "float")
+        
+        peaks, _ = find_peaks(phasic)
+        heights, _, __ = peak_prominences(phasic, peaks)
+        widths, _, __, ___ = peak_widths(phasic, peaks, rel_height=0.63)
+        
+        # find the indices with an amplitude larger that 0.1
+        keep = np.full(len(peaks), True)
+        keep[peaks < 0.1] = False
+        
+        # only keep those
+        peaks=peaks[keep]
+        heights=heights[keep]
+        widths=widths[keep]
+        
+        
+        for i in range(len(peaks)):  
+            onset_amp_index, half_amp_index, offset_amp_index = self.calc_phasic_data(phasic, peaks[i], heights[i])
+            
+            orienting_mag = np.append(orienting_mag, phasic[peaks[i]] - phasic[onset_amp_index])
+            orienting_time = np.append(orienting_time, (offset_amp_index - onset_amp_index)/self.Fs)
+            half_recov_time = np.append(half_recov_time, (half_amp_index - peaks[i])/self.Fs)
+        
+        cv_mg = np.std(orienting_mag)/np.mean(orienting_mag)
+        cv_orient = np.std(orienting_time)/np.mean(orienting_time)
+        cv_recov = np.std(half_recov_time)/np.mean(half_recov_time)
+        
+        temp_array = np.append(temp_array, cv_mg)
+        temp_array = np.append(temp_array, cv_orient)
+        temp_array = np.append(temp_array, cv_recov)
+        
+        if(state == False):
+            temp_array = np.append(temp_array,0)
+        else:
+            temp_array = np.append(temp_array,1)
+
+        return temp_array
+
+# EDA = {}
+# EDA = EDAprep(fs,eda_base,20,"baseline")
    
-EDA.plotdata()
-eda_lp = EDA.filtering_data()
-eda_sm = EDA.smoothing_data(eda_lp)
-EDA.decompose_data(eda_sm)
+# EDA.plotdata()
+# eda_lp = EDA.filtering_data()
+# eda_sm = EDA.smoothing_data(eda_lp)
+# EDA.decompose_data(eda_sm)
 
-EDA = {}
-EDA = EDAprep(eda_stress,10,"stress")
-
-EDA.plotdata()
-eda_lp = EDA.filtering_data()
-eda_sm = EDA.smoothing_data(eda_lp)
-EDA.decompose_data(eda_sm)
-
-EDA = {}
-EDA = EDAprep(eda_amusement,20,"amusement")
-
-EDA.plotdata()
-eda_lp = EDA.filtering_data()
-eda_sm = EDA.smoothing_data(eda_lp)
-EDA.decompose_data(eda_sm)
-
-EDA = {}
-EDA = EDAprep(eda_meditation,20,"meditation")
-
-EDA.plotdata()
-eda_lp = EDA.filtering_data()
-eda_sm = EDA.smoothing_data(eda_lp)
-EDA.decompose_data(eda_sm)  
-'''
