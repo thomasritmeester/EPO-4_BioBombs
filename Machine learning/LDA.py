aa7f01bbc4b9ed8567cd6b711bc1fce40e5f58fd
AccomplishedOffice <40142698+dwbhelp@users.noreply.github.com>
2023-05-31 15:15:30 +0200

    Added EMG Features

--- Machine learning/LDA.py
+++ Machine learning/LDA.py
@@ -18,12 +18,10 @@ print("Start!")
 data_set_path = "D:/Downloads/WESAD/WESAD/"
 subject = ["S2",'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'S11', 'S13', 'S14', 'S15', 'S16', 'S17']
 
-features_base = np.asarray(np.zeros(36), dtype = "float")
-features_stress = np.asarray(np.zeros(36), dtype = "float")
+features_base = np.asarray(np.zeros(50), dtype = "float")
+features_stress = np.asarray(np.zeros(50), dtype = "float")
 
 for i in range(len(subject)):
-    print("subject: ", subject[i])
-
     obj_data = {}
 
     obj_data[subject[i]] = read_data_of_one_subject(data_set_path, subject[i])
@@ -37,6 +35,9 @@ for i in range(len(subject)):
     eda_data_stress=chest_data_dict['EDA'][stress,0]
     eda_data_base=chest_data_dict['EDA'][baseline,0]
 
+    emg_data_stress=chest_data_dict['EMG'][stress,0]
+    emg_data_base=chest_data_dict['EMG'][baseline,0]
+    
     temp_data_stress=chest_data_dict['Temp'][stress,0]
     temp_data_base=chest_data_dict['Temp'][baseline,0]
 
@@ -45,28 +46,25 @@ for i in range(len(subject)):
 
     eda_features_base = calc_eda_features(eda_data_base)
     eda_features_stress = calc_eda_features(eda_data_stress)
+    
+    emg_features_base = calc_emg_features(emg_data_base)
+    emg_features_stress = calc_emg_features(emg_data_stress)
 
     temp_features_base = calc_temp_features(temp_data_base)
     temp_features_stress = calc_temp_features(temp_data_stress)
 
-    ecg_features_time_base = ECG_time_data(ecg_data_base)
-    ecg_features_time_stress = ECG_time_data(ecg_data_stress)
+    ecg_features_base = ECG_data(ecg_data_base)
+    ecg_features_stress = ECG_data(ecg_data_stress)
 
-    ecg_features_freq_base = ECG_freq_data(ecg_data_base)
-    ecg_features_freq_stress = ECG_freq_data(ecg_data_stress)
+    # print(ecg_features_base.shape)
+    # print(ecg_features_stress.shape)
 
-    #print(ecg_features_freq_base)
-    #print(ecg_features_freq_base.shape)
+    # np.reshape(eda_features_stress, (1,-1))
 
-    #print(ecg_features_freq_stress)
-    #print(ecg_features_freq_stress.shape)
+    print(eda_features_stress.shape, emg_features_stress.shape, temp_features_stress.shape,ecg_features_stress.shape)
 
-    np.reshape(eda_features_stress, (1,-1))
-
-    #print(eda_features_stress.shape, temp_features_stress.shape,ecg_features_time_stress.shape, ecg_features_freq_stress.shape)
-
-    features_stress = np.vstack((features_stress, np.hstack((eda_features_stress, temp_features_stress, ecg_features_time_stress, ecg_features_freq_stress))  ))
-    features_base = np.vstack((features_base, np.hstack((eda_features_base, temp_features_base, ecg_features_time_base, ecg_features_freq_base)) ))
+    features_stress = np.vstack((features_stress, np.hstack((eda_features_stress, emg_features_stress, temp_features_stress, ecg_features_stress))  ))
+    features_base = np.vstack((features_base, np.hstack((eda_features_base, emg_features_base, temp_features_base, ecg_features_base)) ))
 
 features_base = features_base[1:,:]
 features_stress = features_stress[1:,:]
