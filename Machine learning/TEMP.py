7758541b284c7c05e931121368ea75aac45a9e3b
AccomplishedOffice <40142698+dwbhelp@users.noreply.github.com>
2023-06-07 12:27:43 +0200

    Updated Temperature Features

--- Machine learning/TEMP.py
+++ Machine learning/TEMP.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def calc_temp_features(data):
 
@@ -6,25 +7,34 @@ def calc_temp_features(data):
   TimeWindow = 30
   WindowSize = SamplingRate*TimeWindow
 
-  features = np.zeros(5)
+  features = np.zeros(6)
 
-  temp_features = np.asarray(np.zeros(5), dtype = "float")
+  #temp_features = np.asarray(np.zeros(6), dtype = "float")
+  temp_features = pd.DataFrame()
 
 
-  for i in range(int(data.size/WindowSize)):
+  t_tot = int(len(data)/WindowSize)
+  for i in range(t_tot):
     data_window = data[WindowSize*i:WindowSize+WindowSize*i]
     mean = np.mean(data_window)
     std = np.std(data_window)
     minimum = np.amin(data_window)
     maximum = np.amax(data_window)
+    d_range = maximum/minimum
     slope = np.polyfit(np.arange(0,WindowSize,1),data_window,1)[0]
     features[0] = mean
     features[1] = std
     features[2] = minimum
     features[3] = maximum
     features[4] = slope
-    temp_features = np.vstack((temp_features,features))
+    features[5] = d_range
 
-  return temp_features[1:,:]
+
+    pd_features = pd.DataFrame([features], columns = ['mean', 'std', 'minimum', 'maximum', 'slope', 'Dynamic Range'])
+
+    temp_features = pd.concat([temp_features, pd_features], ignore_index=True)
+    #temp_features = np.vstack((temp_features,features))
+
+  return temp_features
   
     
