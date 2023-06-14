fa3b71ad8c08b90bafa2b2326794cefda70c051b
jortrk22 <100145329+jrkuiper@users.noreply.github.com>
2023-06-14 14:35:04 +0200

    CrossFold test C4

--- Machine learning/CrossFold Test.py
+++ Machine learning/CrossFold Test.py
@@ -4,13 +4,14 @@ from CrossFold import *
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 all_data_pq = pq.read_table('all_data_df.parquet')
 all_data_df= all_data_pq.to_pandas()
+all_data_df.columns = [x[1] for x in all_data_df.columns]
+print(all_data_df.loc[200,:])
 
-# print(all_data_df.loc[4,:])
+print(all_data_df)
+# subjects=["P1","P2","P5","P6","P9","P10","P11","P12"]
+# lda=LDA(n_components=1)
 
-subjects=["P1","P2","P5","P6","P9","P10","P11","P12"]
-lda=LDA(n_components=1)
-
-accuracy_score, f1_score, mcc_score=CrossFold(subject=subjects, model=lda, all_data_df=all_data_df, data = 'False')
-print("accuracy_score=", accuracy_score, '\n')
-print("f1_score=", accuracy_score, '\n')
-print("mcc_score", accuracy_score, '\n')
+# accuracy_score, f1_score, mcc_score=CrossFold(subject=subjects, model=lda, all_data_df=all_data_df, data = 'False')
+# print("accuracy_score=", accuracy_score, '\n')
+# print("f1_score=", accuracy_score, '\n')
+# print("mcc_score", accuracy_score, '\n')
